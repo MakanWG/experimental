a5eeba09362a1f61c7a985b8f72e677c5362f866
MakanWG <koulibaly.papus@gmail.com>
2020-06-22 01:06:51 -0400

    test wasm

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+console.debug("[ServiceWorker] Initializing");
+
+let config = {};
+
+self.addEventListener('install', function (e) {
+    console.debug('[ServiceWorker] Installing offline worker');
+    e.waitUntil(
+        fetch("./uno-config.js")
+            .then(r => r.text()
+                .then(configStr => {
+                    eval(configStr);
+                    caches.open('managed-6cb685261224af108e60514b6f7cf23d3576304a').then(function (cache) {
+                        console.debug('[ServiceWorker] Caching app binaries and content');
+                        return cache.addAll(config.offline_files);
+                    });
+                }
+                )
+            )
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(async function () {
+        try {
+            // Network first mode to get fresh content every time, then fallback to
+            // cache content if needed.
+            return await fetch(event.request);
+        } catch (err) {
+            return caches.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        }
+    }());
+});
+
+
+// managed-6cb685261224af108e60514b6f7cf23d3576304a
